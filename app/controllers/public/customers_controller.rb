b9faf086378c38053a223ce765585030c4911d02
sabaranran <sabaranran@gmail.com>
2023-02-26 02:22:11 +0000

    認証機能と管理機能一部

---
+++ app/controllers/public/customers_controller.rb
@@ -0,0 +1,28 @@
+class Public::CustomersController < ApplicationController
+  def show
+    @customer = current_customer
+  end
+
+  def edit
+    @customer = current_customer
+  end
+
+  def unsubscribe
+  end
+
+  def update
+    @customer = current_customer
+    if @current_customer.update(customer_params)
+      redirect_to public_customers_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name,:first_name,:last_name_kana,:first_name_kana,:postal_code,:address,:telephone_number,:email)
+  end
+
+end
